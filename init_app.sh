4ab06e43aa880bfc74dd9e70a87f8840edac36a1
shenshuo <191715030@qq.com>
2024-06-14 16:23:21 +0800

    2024年6月12日  feat(readme)  优化token生成

---
+++ init_app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 执行docker命令获取token
+TOKEN=$(docker exec -it codo_mg python3 manage.py token_init)
+
+# 更新或替换.env文件中的CODO_AUTH_KEY变量的值
+sed -i "s/^CODO_AUTH_KEY=.*/CODO_AUTH_KEY=\"$TOKEN\"/" .env
+
+echo "Token成功更新到.env文件的CODO_AUTH_KEY变量中"
+
+echo "开始初始化数据库"
+docker exec -it codo_mg python3 manage.py db_init
+
+echo "开始重启应用"
+docker-compose -f docker-compose-app.yaml restart
+
+echo "开始服务和路由注册"
+sh init_etcd.sh
